42091733ca9c30e5943bf1d44f80367b9eb052bb
WSS-MASTER\dt204842 <SPluamjitta@dstworldwideservices.com>
2017-05-26 18:16:24 +0700

    added vcard

---
+++ src/v-card/V-Card.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import './V-Card.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Card, CardImg, CardText, CardBlock,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+import { openTimeLine, closeTimeLine } from '../actions'
+import { connect } from 'react-redux'
+
+class VCard extends Component{
+
+    constructor(props){
+        super(props)
+        console.dir(props)
+
+        this.openTimeLine = this.props.openTimeLine
+        this.closeTimeLine = this.props.closeTimeLine
+
+    }
+
+//style={{display: this.state.showStore ? '' : 'none' }}
+    render() {
+        return (
+            <div >
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBlock>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button onClick={this.props.openTimeLine}>Open</Button>
+                    <Button onClick={this.props.closeTimeLine}>Close</Button>
+                    <Button onClick={this.Hello}>Alert Status</Button>
+                    </CardBlock>
+                </Card>
+            </div>
+        );
+    }
+
+    Hello = () => {
+        //alert ('TimeLine status : '+ this.props )
+        console.log('TimeLine status : ')
+        console.log(this.props.type )
+    }
+    /*
+    openTimeLine = () => {
+        //this.handlers.dispatch( { type: 'Open'} ) 
+        alert( 'openTimeLine' )
+    }
+    */
+}
+
+const mapStateToProps = (state , ownProps) => {
+    //console.log( ownProps )
+    //console.log( state )
+  return {
+    type: state.timeline
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    openTimeLine: () => {
+        dispatch(openTimeLine())
+    },
+    closeTimeLine: () => {
+        dispatch(closeTimeLine())
+    }
+  }
+}
+
+VCard = connect(mapStateToProps,mapDispatchToProps)(VCard)
+
+export default VCard;
