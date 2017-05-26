361a69937972d6c244bf7506d4fba2aed007eb0e
WSS-MASTER\dt204842 <SPluamjitta@dstworldwideservices.com>
2017-05-26 18:16:46 +0700

    added timelinecard.

---
+++ src/timeline-card/TimeLineCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './TimeLine.css';
+
+class TimeLineCard extends Component {
+
+  constructor(props){
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default TimeLineCard;
